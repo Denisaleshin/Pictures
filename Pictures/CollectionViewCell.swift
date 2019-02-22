b5a7bbef8bfdbab56b4242243fb243c6d7ec458c
Dzianis Alioshyn <Dzianis_Alioshyn@epam.com>
2019-02-22 21:55:32 +0300

    Add separate class for collectionViewCell

---
+++ Pictures/CollectionViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  CollectionViewCell.swift
+//  Pictures
+//
+//  Created by Dzianis Alioshyn on 2/22/19.
+//  Copyright © 2019 myOwn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(priceLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            priceLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            priceLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5),
+            
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            titleLabel.bottomAnchor.constraint(equalTo: priceLabel.topAnchor, constant: 5),
+            
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            imageView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -15)
+            
+            ])
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var titleLabel: UILabel = {
+        Label.makeStandartLabel()
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        Label.makeStandartLabel()
+    }()
+    
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView(frame: .zero)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = UIColor.red
+        return imageView
+    }()
+}
+
+extension CollectionViewCell: Contentable {
+    func setupContetn(title: String) {
+        self.titleLabel.text = title
+    }
+    
+    func setupContetnt(price: Int) {
+        self.priceLabel.text = "\(price)"
+    }
+    
+    func setupContetnt(image: UIImage) {
+        imageView.image = image
+    }
+}
--- Pictures/CollectionViewModel.swift
+++ Pictures/CollectionViewModel.swift
@@ -12,64 +12,28 @@ import UIKit
 protocol Contentable {
     func setupContetn(title: String)
     func setupContetnt(price: Int)
-    func setupContetnt(imageURL: String)
+    func setupContetnt(image: UIImage)
 }
-
-
-
 
 protocol SectionViewModelProtocol {
     var items: [CellItemProtocol] { get }
     func model(for elementOfKind: String) -> CellItemProtocol?
 }
 
-
-
 protocol CellItemProtocol {
     var reuseIndetifier: String { get }
     func setup(_ cell: UICollectionReusableView, in collectionView: UICollectionView, at indexPath: IndexPath)
+    func cancelDownloadingFor(_ cell: UICollectionReusableView, in collectionView: UICollectionView, at indexPath: IndexPath)
 }
 
 
-
-class CollectionViewCell: UICollectionViewCell {
-    var imageURL: String?
-    lazy var titleLabel: UILabel = {
-        Label.makeStandartLabel()
-    }()
-    
-    lazy var priceLabel: UILabel = {
-        Label.makeStandartLabel()
-    }()
-    
-    lazy var imageViev: UIImageView = {
-        let imageView = UIImageView(frame: .zero)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        return imageView
-    }()
-}
-
-extension CollectionViewCell: Contentable {
-    func setupContetn(title: String) {
-        
-        self.titleLabel.text = title
-    }
-    
-    func setupContetnt(price: Int) {
-        self.priceLabel.text = "\(price)"
-    }
-    
-    func setupContetnt(imageURL:String) {
-        self.imageURL = imageURL
-    }
-}
 
 struct Label {
     static func makeStandartLabel() -> UILabel {
         let label = UILabel(frame: .zero)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = label.font.withSize(15)
-        label.textColor = UIColor.black
+        label.textColor = UIColor.black // just to test
         return label
     }
 }
